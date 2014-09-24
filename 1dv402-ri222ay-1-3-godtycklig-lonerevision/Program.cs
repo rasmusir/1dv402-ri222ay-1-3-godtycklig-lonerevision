2134ff9a5fb71aa4864507b83f43d765e3720270
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2014-09-25 00:37:11 +0200

    + MyExtension
    + IsContinuing() : bool
    + ReadInt(string prompt) : int
    + ReadSalaries(int count) : int[]
    + ViewMessage(string message, ConsoleColor backgroundColor = ConsoleColor.Blue, ConsoleColor foregroundColor = ConsoleColor.White) : void
    + ViewResults(int[] salaries) : void

--- 1dv402-ri222ay-1-3-godtycklig-lonerevision/Program.cs
+++ 1dv402-ri222ay-1-3-godtycklig-lonerevision/Program.cs
@@ -10,6 +10,42 @@ namespace _1dv402_ri222ay_1_3_godtycklig_lonerevision
     {
         static void Main(string[] args)
         {
+            do
+            {
+
+            }
+            while (IsContinuing());
+        }
+
+        static bool IsContinuing()
+        {
+            ViewMessage("Tryck tangent för att fortsätta - Esc avsluta.");    // TODO Replace with resource later.
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+                return false;
+            return true;
+        }
+
+        static int ReadInt(string prompt)
+        {
+            throw new NotImplementedException();
+        }
+
+        static int[] ReadSalaries(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void ViewMessage(string message, ConsoleColor backgroundColor = ConsoleColor.Blue, ConsoleColor foregroundColor = ConsoleColor.White)
+        {
+            Console.BackgroundColor = backgroundColor;
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void ViewResults(int[] salaries)
+        {
+            throw new NotImplementedException();
         }
     }
 }
